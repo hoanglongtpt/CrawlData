e5348bb8d60dbfda2bab14c0cb4581146984f851
websitefree100 <websitefree100@gmail.com>
2024-09-28 11:29:05 +0700

    add login ui

--- resources/views/member/auth/login.blade.php
+++ resources/views/member/auth/login.blade.php
@@ -1,6 +1,61 @@
 @extends('member.auth.auth_master')
 @section('content')
-    <div class="container">
+
+    <section class="login-container">
+        <div class="login-card">
+            <div class="side img-side">
+                <img class="login-img"  src="{{asset('assets-theme/img/login.jpg')}}">
+            </div>
+            <div class="side login-side">
+                <div class="logo">LOGO</div>
+                <div class="login-wrapper">
+                    <div class="login-text">Đăng nhập</div>
+                    <form action="{{route('member.postLogin')}}"  class="user" method="POST">
+                        @csrf
+                        <div class="form-group">
+                            <input type="email" name="email" class="form-control-login"
+                                id="exampleInputEmail" aria-describedby="emailHelp"
+                                placeholder="Nhập email của bạn">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" name="password" class="form-control-login"
+                                id="exampleInputPassword" placeholder="Nhập mật khẩu">
+                        </div>
+                        <div class="form-group submit-group">
+                            <!-- <input type="checkbox" class="form-control-checkbox" id="customCheck"> -->
+                            <div class="custom-control custom-checkbox small">
+                                <input type="checkbox" class="custom-control-input" id="customCheck">
+                                <label class="custom-control-label" for="customCheck">Ghi nhớ</label>
+                            </div>
+                            <button  type="submit" href="index.html" class="login-btn">Đăng nhập</button>
+                        </div>
+                        <hr class="divider">
+                        <a href="index.html" class="btn btn-google btn-user btn-block social-btn">
+                            <img width="24px" height="24px" src="{{asset('assets-theme/img/google.svg')}}">
+                            Đăng nhập bằng Google
+                        </a>
+                        <a href="index.html" class="btn btn-facebook btn-user btn-block social-btn">
+                            <img width="24px" height="24px" src="{{asset('assets-theme/img/facebook.svg')}}">
+                            Đăng nhập bằng Facebook
+                        </a>
+                    </form>
+                    <hr class="divider">
+                    <div class="question-group">
+                        <a>Quên mật khẩu?</a>
+                        <a>Tạo tài khoản</a>
+                    </div>
+                    <div class="policy-group">
+                        <p>Chính sách bảo mật</p>
+                        <span>|</span>
+                        <span class="hotline-text">Hotline:058.789.6799</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <div class="hidden container login-container">
 
         <!-- Outer Row -->
         <div class="row justify-content-center">
@@ -11,7 +66,9 @@
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
-                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                            <div class="col-lg-6 d-none d-lg-block login-img-wrapper">
+                                <img class="login-img"  src="{{asset('assets-theme/img/login.jpg')}}">
+                            </div>
                             <div class="col-lg-6">
                                 <div class="p-5">
                                     <div class="text-center">
