ff8baea726411434fd5bc99adbda6c0e9fd9f036
hoanglongtpt <hoanglongtpt1999@gmail.com>
2024-10-23 23:29:45 +0700

    done up Freepik

--- app/Extensions/Extension.php
+++ app/Extensions/Extension.php
@@ -37,5 +37,36 @@ class Extension
             return "";
         }
     }
+
+    public static function GetTypeDowload ($link) {
+        if (self::hasPremiumKeyword($link)) {
+            return 'premium';
+        }elseif (self::hasVideoKeyword($link)) {
+            return 'video';
+        }else{
+            return 'normal';
+        } 
+    }
+
+    private static function hasPremiumKeyword($url) {
+        $prefix = 'https://www.freepik.com/';
+        if (strpos($url, $prefix) === 0) {
+            $remainingPart = substr($url, strlen($prefix));
+            
+            return strpos($remainingPart, 'premium-') !== false;
+        }
+        return false; 
+    }
+
+    private static function hasVideoKeyword($url) {
+        $prefix = 'https://www.freepik.com/';
+        if (strpos($url, $prefix) === 0) {
+            $remainingPart = substr($url, strlen($prefix));
+            
+            return strpos($remainingPart, 'video') !== false;
+        }
+        return false; 
+    }
+    
 }
 
